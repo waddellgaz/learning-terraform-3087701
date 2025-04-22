057d2db639957900c06a2cb191d2094d64d2931b
Gary <waddellgaz@gmail.com>
2025-04-22 13:57:27 +0100

    autoscaling

--- main.tf
+++ main.tf
@@ -29,17 +29,6 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id              = module.blog_vpc.public_subnets[0]
-
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
 
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
@@ -90,15 +79,13 @@ module "blog_alb" {
   }
 }
 
-
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.3.0"
-  name    = "blog"
+  version = "4.13.0"
 
-  vpc_id = module.blog_vpc.vpc_id
-
-  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  vpc_id              = module.blog_vpc.vpc_id
+  name                = "blog"
+  ingress_rules       = ["https-443-tcp", "http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
