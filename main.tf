aae609c662e64565da311895387fad33cef9f8d3
Gary <waddellgaz@gmail.com>
2025-04-22 11:41:09 +0100

    patch

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ module "blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
     Terraform = "true"
