91addcdaa8f9a10ec0c786678ec0d0896413aa72
Gary <waddellgaz@gmail.com>
2025-04-22 11:17:15 +0100

    Patch

--- main.tf
+++ main.tf
@@ -38,8 +38,8 @@ module "blog_sg" {
 
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress = ["all-all"]
-  egress = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog"{
